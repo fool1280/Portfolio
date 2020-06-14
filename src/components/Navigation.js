b6e2ee73db183f1bcab687017b620a644019522d
fool1280 <anhnguyen@Anhs-MBP.attlocal.net>
2020-06-14 17:01:45 -0500

    final

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,9 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
 export default function Navigation() {
+    let openNewTab = (url) => {
+        let win = window.open(url, '_blank');
+    }
     return (
         <div>
             <Navbar variant="dark" expand="lg" style={{backgroundColor: "rgba(0, 0, 0)", fontSize: "20px"}} sticky="top">
@@ -13,7 +16,7 @@ export default function Navigation() {
                     <Nav.Link as={Link} to="/about" >About</Nav.Link>
                     <Nav.Link as={Link} to="/projects" >Projects</Nav.Link>
                     <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
-                    <Nav.Link href="https://anhnguyenhenry.myportfolio.com/">Photography</Nav.Link>
+                    <Nav.Link type="button" onClick={() => openNewTab("https://anhnguyenhenry.myportfolio.com/")}>Photography</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Navbar>
